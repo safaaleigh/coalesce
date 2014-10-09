a786378bc86544d023d60312c6462aa273577e20
safaaleigh <safaalhansen@gmail.com>
2014-10-09 17:20:21 -0500

    Can add events and event rules with durations

--- Coalesce.java
+++ Coalesce.java
@@ -7,25 +7,32 @@ import java.util.ArrayList;
 
 public class Coalesce {
 	public static void main(String[] args) {
-		CoalesceCalendar cal = new CoalesceCalendar();
+		CoalesceCalendar coacal = new CoalesceCalendar();
 		int[] startTime = {2013,10,9,9,27};
-		cal.addEvent(new Event("Algorithms and Complexity", 3.0));
+		coacal.addEvent(new Event("Algorithms and Complexity", 3.0));
 		int[] onDays = {0,1,2,3,4,5,6};
-		int[][] onHm = {{12,0}};
-		cal.addEventRule(new EventRule("Lunch", onDays, onHm));
+		int[][] onTimes = {{12,0}};
+		coacal.addEventRule(new EventRule("Lunch", onDays, onTimes, 1));
+		coacal.generate();
+
+		System.out.println(coacal);
 	}
 }
 
 class CoalesceCalendar {
 	Map<String,Event> scheduledEvents = new HashMap<String,Event>();
 	ArrayList<EventRule> eventRules = new ArrayList<EventRule>();
+	Calendar week;
 
+	public CoalesceCalendar() {
+		week = Calendar.getInstance();
+	}
 	public void addEvent(Event e) {
-		scheduledEvents.put(e.getID(), e);
+		scheduledEvents.put(e.getTitle(), e);
 	}
 
 	public void removeEvent(Event e) {
-		scheduledEvents.remove(e.getID());
+		scheduledEvents.remove(e.getTitle());
 	}
 
 	public Event getEvent(String title) {
@@ -35,27 +42,55 @@ class CoalesceCalendar {
 	public void addEventRule(EventRule er) {
 		eventRules.add(er);
 	}
+
+	public void generate() {
+		for (EventRule er : eventRules) {
+			er.applyRule(scheduledEvents, week);
+		}
+	}
+
+	public String toString() {
+		String result= "Calendar this week:\n";
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+		for (Event e : scheduledEvents.values()) {
+			result += e.getTitle() + " on " + dateFormat.format(e.getStart().getTime()) + (e.getDuration() != 0 ? " for " + e.getDuration() + " hour(s)" : "") + ".\n";
+		}
+		return result;
+	}
 }
 
 class Event {
-	private double duration;
 	private boolean mutable;
-	private Calendar timeBlock;
+	private Calendar startTimeAndDay;
+	private double duration;
 	private String title;
 
 	public Event(String et, double dur) {
+		startTimeAndDay = Calendar.getInstance();
 		duration = dur;
 		title = et;
 	}
 
-	String getID() {
+	public Event(String et, Calendar st, double dur) {
+		title = et;
+		startTimeAndDay = st;
+		duration = dur;
+	}
+
+	public String getTitle() {
 		return title;
 	}
 
-	public String toString() {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy MMM dd HH:mm:ss");
+	public double getDuration() {
+		return duration;
+	}
 
-		return title + "\n" + sdf.format(timeBlock.getTime());
+	public Calendar getStart() {
+		return startTimeAndDay;
+	}
+
+	public String toString() {
+		return "";
 	}
 }
 
@@ -80,19 +115,72 @@ class HourMinute {
 	}
 }
 
+
 class EventRule {
 	private ArrayList<Integer> days = new ArrayList<Integer>();
 	private ArrayList<HourMinute> times = new ArrayList<HourMinute>();
+	private double duration;
 	private String name;
 
-	public EventRule(String eventRuleName, int[] onDays, int[][] onHm) {
+	public EventRule(String eventRuleName, int[] onDays, int[][] onTimes, double dur) {
 		name = eventRuleName;
 		for ( int i = 0; i < onDays.length; i++ ) {
 			days.add(onDays[i]);
 		}
-		for ( int j = 0; j < onHm.length; j++ ) {
-			times.add(new HourMinute(onHm[j][0],onHm[j][1]));
+		for ( int j = 0; j < onTimes.length; j++ ) {
+			times.add(new HourMinute(onTimes[j][0],onTimes[j][1]));
 		}
+		duration = dur;
+	}
+
+	public void applyRule(Map<String,Event> events, Calendar week) {
+		int i = 0;
+		for (Integer d : days) {
+			for (HourMinute hm : times) {
+				Calendar ecal = week;
+				ecal.set(Calendar.DAY_OF_WEEK, d+1);
+				ecal.set(Calendar.HOUR_OF_DAY, hm.hour);
+				ecal.set(Calendar.MINUTE, hm.minute);
+				events.put(name + "" + i, new Event(name, ecal, duration));
+				i++;
+			}
+		}
+	}
+
+	public String toString() {
+		String result = name + " occurs on ";
+		for (int i = 0; i < days.size(); i++) {
+			switch(days.get(i)) {
+				case 0:
+					result += "Monday ";
+					break;
+				case 1:
+					result += "Tuesday ";
+					break;
+				case 2:
+					result += "Wednesday ";
+					break;
+				case 3:
+					result += "Thursday ";
+					break;
+				case 4:
+					result += "Friday ";
+					break;
+				case 5:
+					result += "Saturday ";
+					break;
+				case 6:
+					result += "Sunday ";
+					break;
+				default:
+					break;
+			}
+		}
+		result += "at ";
+		for (int i = 0; i < times.size(); i++) {
+			result += times.get(i).toString();
+		}
+		return result + "\n";
 	}
 }
 
